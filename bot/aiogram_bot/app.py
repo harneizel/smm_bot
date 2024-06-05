7b109971704c8a6fca104cc6b02569aa52519a6b
harneizel <genyievg@gmail.com>
2024-06-06 00:03:47 +0400

    admin filter

--- bot/aiogram_bot/app.py
+++ bot/aiogram_bot/app.py
@@ -1,13 +1,14 @@
 import logging
 
-from aiogram import Dispatcher, Bot
+from aiogram import Dispatcher, Bot, F
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from bot.aiogram_bot.misc.filters import register_filters
 from bot.aiogram_bot.misc.middlewares import register_middlewares
 from bot.database.models import on_startup_database
 from bot.utils.config import TG_TOKEN
-
+from bot.aiogram_bot.misc.filters.filters import IsAdmin
+from bot.aiogram_bot.handlers.admin import admin_panel
 
 async def aiogram_on_startup(bot: Bot):
     await on_startup_database()
@@ -23,15 +24,18 @@ def register_routers(dp: Dispatcher):
     from bot.aiogram_bot.handlers import menu, admin
     dp.include_routers(
         menu.router,
-        #admin.router
+        admin.router,
     )
+
+
+#def register_filters(dp):
 
 
 async def aiogram_start():
     bot = Bot(token=TG_TOKEN)
     dp = Dispatcher(storage=MemoryStorage())
 
-    #register_middlewares(dp)
+    # register_middlewares(dp)
     #register_filters(dp)
 
     register_routers(dp)
--- bot/aiogram_bot/handlers/admin.py
+++ bot/aiogram_bot/handlers/admin.py
@@ -7,10 +7,10 @@ from bot.database import requests as rq
 from bot.utils.config import CHANNEL_ID, PAYMENTS_TOKEN, PRICE, BASIC_LIMIT, PAID_LIMIT, ADMIN_IDS
 import bot.aiogram_bot.markups.inline.inline_kb as inline_kb
 import bot.texts as text
-from bot.aiogram_bot.misc.filters.is_admin import IsAdmin
+from bot.aiogram_bot.misc.filters.filters import *
 admin_ids = [6419408258]
 router = Router()
 
-@router.message(IsAdmin(), F.text == "/admin")
+@router.message(F.text == "/admin", IsAdmin())
 async def admin_panel(message: Message, bot: Bot):
     await message.answer('Вы зашли в админ панель')
