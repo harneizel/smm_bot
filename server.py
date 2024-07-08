56ca39902d624843ac11c81b2ca99b3a2555dfd9
harneizel <genyievg@gmail.com>
2024-07-08 05:55:16 +0400

    фиксы

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 import hashlib
-import json.dumps as dumps
+import json
 import asyncio
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import uvicorn
 
 from bot.utils.config import MRH_LOGIN, PASS_2
 from bot.database import requests as rq
@@ -25,8 +27,7 @@ async def get_payment():
     Id = request.form.get('Shp_id')  # tg_id клиента
     print(OutSum, InvId, Fee, EMail, SignatureValue, PaymentMethod, IncCurrLabel, Id)
     pass_2 = PASS_2
-    SignatureIntended = hashlib.md5(
-        f"{OutSum}:{InvId}:{pass_2}:Shp_id={Id}".encode('utf-8')).hexdigest()  # предполагаемая подпись
+    SignatureIntended = hashlib.md5(f"{OutSum}:{InvId}:{pass_2}:Shp_id={Id}".encode('utf-8')).hexdigest()  # предполагаемая подпись
     SignatureIntended = SignatureIntended.upper()
     print(SignatureValue, SignatureIntended)
     if SignatureIntended == SignatureValue:
@@ -35,20 +36,38 @@ async def get_payment():
         return f"OK{InvId}"
     print("подписи не совпали")
 
-@app.get('/send-app')
-def send_app():
-    print("Приложение отправлено")
+@app.get('/app')
+def open_app():
+    print("Приложение открыто")
+    with open('./web-app/index.html', 'r') as file:
+        html_content = file.read()
+    return HTMLResponse(content=html_content, status_code=200)
+
+@app.get('/app/knowledge')
+def open_app():
+    print("knowledge открыто")
+    with open('./web-app/knowledge.html', 'r') as file:
+        html_content = file.read()
+    return HTMLResponse(content=html_content, status_code=200)
+
+@app.get('/app/networking')
+def open_app():
+    print("networking открыт")
+    with open('./web-app/networking.html', 'r') as file:
+        html_content = file.read()
+    return HTMLResponse(content=html_content, status_code=200)
 
 @app.get('/user_info/{user_id}')
 async def user_info(user_id: int):
     print(user_id)
     user_data = await rq.get_user_data(user_id)
-    data = [('desc', user_data[0]), ('tag1', user_data[1]), ('tag2', user_data[2]), ('tag3', user_data[3]), ('tag4', user_data[4]),('tag5', user_data[5])]
-    json_data = dumps(data)
+    data = [('desc', user_data[0]), ('tag1', user_data[1]), ('tag2', user_data[2]),
+            ('tag3', user_data[3]), ('tag4', user_data[4]),('tag5', user_data[5])]
+    json_data = json.dumps(data)
     return json_data
 
-if __name__ == '__main__':
+'''if __name__ == '__main__':
     try:
-        app.run(host="0.0.0.0")  # запуск сервера
+         # запуск сервера
     except:
-        print("Завершение работы")
+        print("Завершение работы")'''
